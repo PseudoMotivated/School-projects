ae2b6c4dbd28e676f4beef25a631eff1892e8d78
PsuedoMotivated <122965701+PsuedoMotivated@users.noreply.github.com>
2023-01-21 16:45:46 +0100

    Added comments and made it look neater.

--- Diode project/Traffic_light.ino
+++ Diode project/Traffic_light.ino
@@ -1,10 +1,10 @@
-//Defines what pins bwlong to what color diodes, so I dont get a headache when I go back looking over the code trying to see what pin does what.
+// Defines what pins bwlong to what color diodes, so I dont get a headache when I go back looking over the code trying to see what pin does what.
 const int red = 0;
 const int yellow = 1;
 const int green = 2;
 const int button = 3;
 
-//Sets a start-value for the button state. To avoid errors down the road.
+// Sets a start-value for the button state. To avoid errors down the road.
 int buttonState = 0;
 
 void setup() {
@@ -12,19 +12,18 @@ void setup() {
   pinMode(yellow, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(button, INPUT);
-//Every digital pin in the arduino has a resistor in it, the line below enables that resistor, getting rid of the need of a physical one.  
+// Every digital pin in the arduino has a resistor in it, the line below enables that resistor, getting rid of the need of a physical one.  
   digitalWrite(button, HIGH);
 }
-//Counts from 1 to 3, increments for every button press. Resets to 1 when it reaches 4. 
 
-//Makes the red light blink
+// Makes the red light blink
 void redlight() {
     digitalWrite(0, HIGH);
     delay(300);
     digitalWrite(0, LOW);
     delay(300);
 }
-//Makes the yellow light blink
+// Makes the yellow light blink
 void yellowlight() {
     digitalWrite(1, HIGH);
     delay(300);
@@ -32,7 +31,7 @@ void yellowlight() {
     delay(300);
     
 }
-//Makes the green light blink
+// Makes the green light blink
 void greenlight() {
     digitalWrite(2, HIGH);
     delay(300);
@@ -42,21 +41,25 @@ void greenlight() {
 
 // Equivalent to main
 void loop(){
-//Counts from 1 to 3, increments for every button press. Resets to 1 when it reaches 4. 
+// Counts from 1 to 3, increments for every button press. Resets to 1 when it reaches 4. 
   int i = 0;
   buttonState == digitalRead(button);
   if (buttonState = LOW) {
     i + 1;
+// Resets the counter to 1 when it reaches 4. Going back to the red light.
     if (i >= 4) {
        i = 1;
     }
   }
+// Loops the redlight function when the counter reaaches 1. 
   while (i == 1) {
     redlight();
   }
+// Loops the yellowlight function when the counter reaaches 2. 
   while (i == 2) {
     yellowlight();
   }
+// Loops the greenlight function when the counter reaaches 3. 
   while (i == 3) {
     greenlight();
   }
